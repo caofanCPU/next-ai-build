4a8526f0da1f6eaf6010bc6abbf2c2df38e18bbe
D8GER <xyb5to0zcy@gmail.com>
2026-03-28 13:03:08 +0800

    feat(blog): adjust page, no website footer

---
+++ apps/ddaas/src/app/[locale]/(content)/layout.tsx
@@ -0,0 +1,60 @@
+import { baseOptions, homeNavLinks, levelNavLinks } from '@/app/[locale]/layout.config';
+import { showBanner, localePrefixAsNeeded, defaultLocale } from '@/lib/appConfig';
+import { fingerprintConfig } from '@windrun-huaiin/backend-core/lib';
+import { FingerprintProvider } from '@third-ui/clerk/fingerprint';
+import { CustomHomeLayout } from '@third-ui/fuma/base';
+import type { HomeLayoutProps } from 'fumadocs-ui/layouts/home';
+import type { ReactNode } from 'react';
+
+async function homeOptions(locale: string): Promise<HomeLayoutProps> {
+  return {
+    ...(await baseOptions(locale)),
+    links: [
+      ...(await levelNavLinks(locale)),
+      ...(await homeNavLinks(locale)),
+    ],
+  };
+}
+
+export default async function Layout({
+  params,
+  children,
+}: {
+  params: Promise<{ locale: string }>;
+  children: ReactNode;
+}) {
+  const { locale } = await params;
+  const customeOptions = await homeOptions(locale);
+
+  const homeLayoutOptions: HomeLayoutProps = {
+    ...customeOptions,
+    searchToggle: {
+      enabled: false,
+    },
+    themeSwitch: {
+      enabled: true,
+      mode: 'light-dark-system',
+    },
+  };
+
+  return (
+    <FingerprintProvider config={fingerprintConfig}>
+      <CustomHomeLayout
+        locale={locale}
+        localePrefixAsNeeded={localePrefixAsNeeded}
+        defaultLocale={defaultLocale}
+        options={homeLayoutOptions}
+        showBanner={showBanner}
+        showFooter={false}
+        floatingNav={true}
+        actionOrders={{
+          desktop: ['search', 'theme', 'github', 'i18n', 'secondary'],
+          mobileBar: ['search', 'pinned', 'menu'],
+          mobileMenu: ['theme', 'i18n', 'separator', 'secondary', 'github'],
+        }}
+      >
+        {children}
+      </CustomHomeLayout>
+    </FingerprintProvider>
+  );
+}
