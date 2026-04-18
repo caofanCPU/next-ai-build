36225eea459daf339b689df1c225e7fbd1de0f4b
D8GER <xyb5to0zcy@gmail.com>
2026-04-18 20:30:57 +0800

    docs(license): update

--- apps/ddaas/src/app/[locale]/(clerk)/waitlist/[[...waitlist]]/page.tsx
+++ apps/ddaas/src/app/[locale]/(clerk)/waitlist/[[...waitlist]]/page.tsx
@@ -1,7 +1,7 @@
 /**
  * @license
  * MIT License
- * Copyright (c) 2025 D8ger
+ * Copyright (c) 2026 D8ger
  * 
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
