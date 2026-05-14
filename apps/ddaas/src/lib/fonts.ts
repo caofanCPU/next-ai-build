4e6c6a850ac80ed8cf4d57987350e201a9537ba8
D8GER <xyb5to0zcy@gmail.com>
2026-05-14 16:02:40 +0800

    fix(fonts): update comment to clarify local font usage

--- apps/ddaas/src/lib/fonts.ts
+++ apps/ddaas/src/lib/fonts.ts
@@ -1,6 +1,6 @@
 import localFont from 'next/font/local';
 
-// Just use local font，no more remote Google-font。
+// Just use local font，no more remote Google-font
 export const montserrat = localFont({
   src: [
     { path: '../../public/asserts/Montserrat-Regular.otf', weight: '400', style: 'normal' },
